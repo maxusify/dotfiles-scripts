eaaa8ff0185f829679e30d304639112fdca33035
maxusify <maxusify@gmail.com>
2022-08-27 14:06:58 +0200

    Gnome Screenshot Tool scripts

---
+++ gnome-screenshot/config.sh
@@ -0,0 +1,5 @@
+# Screenshots are sorted by current year and month
+export SORT_BY_DATE=$(date +%Y-%m)
+export SCRSHT_FOLDER="$HOME/Obrazy/Screenshots/$SORT_BY_DATE"
+export SCRSHT_DATE=$(date +%Y-%m-%d-%H:%M:%S)
+export SCRSHT_NAME="Screenshot-$SCRSHT_DATE.jpg"
---
+++ gnome-screenshot/ssarea.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Import date and destination folder
+CONFIG_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$CONFIG_DIR/config.sh"
+
+# Check for directory
+mkdir -p $SCRSHT_FOLDER
+
+# Window screenshot
+gnome-screenshot -a -f $SCRSHT_FOLDER/$SCRSHT_NAME
